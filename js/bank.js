4c2e30a67e67848c1bf384288bb4da134b357e2b
masumbillah360 <masumbillah6584@gmail.com>
2022-08-16 19:05:33 +0600

    added balance validation

--- js/bank.js
+++ js/bank.js
@@ -2,22 +2,35 @@ const dipositAmount = document.getElementById('d-amount');
 const widthdrawAmount = document.getElementById('w-amount');
 const dipositAmountContainer = document.getElementById('dAmount');
 const widthdrawAmountContainer = document.getElementById('wAmount');
+const mainBalance = document.getElementById("mainBalance");
+
+
 document.getElementById('d-btn').addEventListener('click',()=>{
-    const amountString = dipositAmountContainer.innerText;
-    const dAmount = parseFloat(amountString);
-    const inputAmountString = dipositAmount.value;
-    const inputAmount = parseFloat(inputAmountString);
-    const totalAmount = dAmount + inputAmount;
-    dipositAmountContainer.innerText= totalAmount; 
-    dipositAmount.value = '';
+    const totalAmount = dipositHelper(dipositAmountContainer.innerText, dipositAmount.value);
+    if (isNaN(totalAmount)) {
+        alert("Invalied Amount");
+    }
+    else{
+        dipositAmountContainer.innerText= totalAmount; 
+        dipositAmount.value = '';
+    }
 
 });
 document.getElementById('w-btn').addEventListener('click',()=>{
-    const amountString = widthdrawAmountContainer.innerText;
-    const wAmount = parseFloat(amountString);
-    const inputAmountString = widthdrawAmount.value;
-    const inputAmount = parseFloat(inputAmountString);
-    const totalAmount = wAmount + inputAmount;
-    widthdrawAmountContainer.innerText= totalAmount; 
-    widthdrawAmount.value = '';
+    const totalAmount = dipositHelper(widthdrawAmountContainer.innerText, widthdrawAmount.value);
+    if (isNaN(totalAmount)) {
+        alert("invalied");
+    }
+    else{
+        widthdrawAmountContainer.innerText= totalAmount; 
+        widthdrawAmount.value = '';
+    }
 });
+
+    
+const dipositHelper = (inputValue, innerText) =>{
+    inputValue = parseFloat(inputValue);
+    innerText = parseFloat(innerText);
+    return inputValue + innerText;
+    
+};

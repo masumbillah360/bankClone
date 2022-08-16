a5a91937cfcb860a2bf1c743b9ce8c257c1dc3ad
masumbillah360 <masumbillah6584@gmail.com>
2022-08-16 16:15:26 +0600

    added user verify option on login

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log("Connected");
+const logBtn = document.getElementById('logBtn');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+logBtn.addEventListener('click',()=>{
+    const userEmail = email.value;
+    const userPassword = password.value;
+    if (userEmail === 'masum@gmail.com' && userPassword === 'masum') {
+        logBtn.removeAttribute('disabled',true); 
+        window.location.href = './bank.html';
+    }
+    else{
+        alert(`Your Password or Email is invalied \n Please try again.`);
+    }
+
+})

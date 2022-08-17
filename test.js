3e9be6585151d7a5cce0c6ccac5d69a0eb088d21
masumbillah360 <masumbillah6584@gmail.com>
2022-08-17 16:16:22 +0600

    update style and verification

--- test.js
+++ test.js
@@ -1,29 +1,12 @@
-// const adder = (a,b) =>{
-//     return{
-//         a,
-//         b
-//     }
+// it's not proper way to validation  it's simple valiedation for testing purpose 
+// const user ={   
+//         email : ['masum@gmail.com','jui@gmail.com'],
+//         password : ['masum','jui']
 // }
-
-// // console.log(adder(23,43));
-// // const numbers = adder(2,4);
-// // console.log(numbers.a);
-
-
-// const newNumber = () => {
-//     let numbers = adder(4,5);
-//     console.log(numbers.a); 
+// console.log(user.email);
+// if (user.email.includes('masum@gmail.com')) {
+//     console.log("first")
 // }
-// newNumber();
-
-const user ={   
-        email : ['masum@gmail.com','jui@gmail.com'],
-        password : ['masum','jui']
-}
-console.log(user.email);
-if (user.email.includes('masum@gmail.com')) {
-    console.log("first")
-}
-else{
-    console.log("second")
-}
+// else{
+//     console.log("second")
+// }
